64a35593b7e3efbb52877250fb8ac8ec013104f9
Curtis <Trilec@users.noreply.github.com>
2026-08-07 15:32:42 +1200

    Expose deterministic IML library pair builder

--- Utilities/SymbolPicker/SymbolPickerImlExport.h
+++ Utilities/SymbolPicker/SymbolPickerImlExport.h
@@ -16,6 +16,14 @@ String BuildSymbolPickerUppImlLibraryHeader(const SymbolPickerProject& project,
 	const String& iml_file_name,
 	Vector<String>* warnings = nullptr);
 
+bool BuildSymbolPickerUppImlLibrary(const SymbolPickerProject& project,
+	const SymbolPickerCatalog& catalog,
+	SymbolPickerExportScope scope,
+	const String& iml_file_name,
+	String& iml_text,
+	String& header_text,
+	Vector<String>* warnings = nullptr);
+
 bool RunSymbolPickerImlExportSmokeTests(const SymbolPickerCatalog& catalog, String& error);
 
 }
